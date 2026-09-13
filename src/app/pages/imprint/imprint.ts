d9a52848e7893bf499e91dabe61866dafcd6f010
K-Micha <Micha.kuehn91@gmail.com>
2026-09-13 12:41:35 +0200

    add imprint and privacy layouts

--- src/app/pages/imprint/imprint.ts
+++ src/app/pages/imprint/imprint.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Header } from '../../components/workspace/header/header';
+import { Icon } from '../../shared/icon/icon';
 
 @Component({
   selector: 'app-imprint',
-  imports: [],
+  imports: [Header, Icon],
   templateUrl: './imprint.html',
   styleUrl: './imprint.scss',
 })
-export class Imprint {}
+export class Imprint {
+  private readonly location = inject(Location);
+
+  protected goBack(): void {
+    this.location.back();
+  }
+}
--- src/app/pages/privacy/privacy.ts
+++ src/app/pages/privacy/privacy.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Header } from '../../components/workspace/header/header';
 
 @Component({
   selector: 'app-privacy',
-  imports: [],
+  imports: [Header],
   templateUrl: './privacy.html',
   styleUrl: './privacy.scss',
 })
